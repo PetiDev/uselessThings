3520d440106d70bbf89653bcbc06dd23ca3b0e24
ICreeper12 <54474482+ICreeper12@users.noreply.github.com>
2021-10-06 20:41:26 +0200

    better logging

--- primeNumberFinder/main.py
+++ primeNumberFinder/main.py
@@ -5,20 +5,22 @@ numList = {}
 out = ""
 
 print("Generating list...")
-system('cls')
+
 
 for x in range(2, maxNum + 1):
     numList[x] = x
-
+system('cls')
+print("Sorting, may take some time...")
 for _, val in list(numList.items()):
     currNum = val
     for key, value in list(numList.items()):
         if currNum in numList:
             if (currNum != value) and value % numList[currNum] == 0:
                 del numList[value]
-
+system('cls')
+print("Joining numbers, may take some time...")
 for num in numList:
     out += str(num) + " "
-
+system('cls')
 print(out)
 input("PAUSE (press enter to exit)")
